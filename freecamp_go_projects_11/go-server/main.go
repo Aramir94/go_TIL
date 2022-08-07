294a9f989f4f73bd6fc3732cfec4779a5c61b497
Aramir-A1DATAINT <jeonghyunsuk777@gmail.com>
2022-08-08 06:34:23 +0900

    simple webserver

---
+++ freecamp_go_projects_11/go-server/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprint(w, "ParseForm() err: %v\n", err)
+		return
+	}
+	fmt.Fprintf(w, "POST request sucessful\n")
+	name := r.FormValue("name")
+	address := r.FormValue("Address")
+	fmt.Fprintf(w, "Name: %s\n", name)
+	fmt.Fprintf(w, "Address: %s\n", address)
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "Not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	fmt.Fprint(w, "Hello, World!")
+}
+
+func main() {
+	fileserver := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileserver)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Printf("Starting server on port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
