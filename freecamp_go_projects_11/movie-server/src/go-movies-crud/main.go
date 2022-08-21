61191603d16e8e5e46a8db3ce010779c6bf92809
Aramir-A1DATAINT <jeonghyunsuk777@gmail.com>
2022-08-22 07:25:32 +0900

    curd movies go

---
+++ freecamp_go_projects_11/movie-server/src/go-movies-crud/main.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+type Movie struct {
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"` //pointer of Director
+}
+
+type Director struct {
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
+}
+
+var movies []Movie
+
+func getMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
+	json.NewEncoder(w).Encode(movies)
+}
+
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
+	params := mux.Vars(r) //get params
+
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			break
+		}
+		json.NewEncoder(w).Encode(movies)
+	}
+}
+
+/* golang Array and slice
+Array
+var movies [5] int  = [5] int {1,2,3,4,5}
+var movies = [3]int{1,2,3}
+추가 수정 불가
+
+slice : 동적 배열 타입 실행 시점에 결정해서 그럼
+var movies []int = []int{1,2,3} // 대괄호에 아무것도 정해주지 않음
+추가 수정 가능
+
+append : 배열을 자르거나 지우거나 추가하는 기능 : 겁나 빨라잉
+*/
+
+func getMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
+	params := mux.Vars(r) //get params
+	for _, item := range movies {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+}
+
+func createMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
+	var movie Movie
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	movie.ID = strconv.Itoa(rand.Intn((1000000)))
+	movies = append(movies, movie)
+	json.NewEncoder(w).Encode(movie)
+}
+
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
+	params := mux.Vars(r) //get params
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = params["id"]
+			movies = append(movies, movie)
+			json.NewEncoder(w).Encode(movie)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(movies)
+}
+
+func main() {
+	r := mux.NewRouter()
+	movies = append(movies, Movie{ID: "1", Isbn: "448743", Title: "The Godfather", Director: &Director{FirstName: "Francis", LastName: "Coppola"}})
+	movies = append(movies, Movie{ID: "2", Isbn: "448744", Title: "The Godfather: Part II", Director: &Director{FirstName: "Francis", LastName: "Coppola"}})
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Printf("Staring serrver at port:8000\n")
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
