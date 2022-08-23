6c81ca501027ccb4df0d9de1ffdb26283b10ad6c
Aramir-A1DATAINT <jeonghyunsuk777@gmail.com>
2022-08-23 09:19:47 +0900

    db connectore

---
+++ freecamp_go_projects_11/Bookstore/package/config/app.go
@@ -0,0 +1,22 @@
+package confing
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/msyql"
+)
+
+var (
+	db *gorm.DB
+)
+
+func Connect() {
+	d, err := gorm.Open("mysql", "root:dataint/simplerest?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		panic(err)
+	}
+	db = d
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
