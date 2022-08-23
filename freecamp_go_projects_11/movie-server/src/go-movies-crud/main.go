1cb17d58dd157e8df6817cabd77d9e92109528bd
Aramir-A1DATAINT <jeonghyunsuk777@gmail.com>
2022-08-23 09:12:35 +0900

    bookstore

--- freecamp_go_projects_11/movie-server/src/go-movies-crud/main.go
+++ freecamp_go_projects_11/movie-server/src/go-movies-crud/main.go
@@ -82,6 +82,7 @@ func updateMovie(w http.ResponseWriter, r *http.Request) {
 	for index, item := range movies {
 		if item.ID == params["id"] {
 			movies = append(movies[:index], movies[index+1:]...)
+
 			var movie Movie
 			_ = json.NewDecoder(r.Body).Decode(&movie)
 			movie.ID = params["id"]
